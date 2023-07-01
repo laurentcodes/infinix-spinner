c5de4e5ab7a775eaf852ef86d251aa2ac711a391
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-01 09:08:41 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	headers: () => [
-		{
-			source: '/:path*',
-			headers: [
-				{
-					key: 'Cache-Control',
-					value: 'no-store',
-				},
-			],
-		},
-	],
+	// headers: () => [
+	// 	{
+	// 		source: '/:path*',
+	// 		headers: [
+	// 			{
+	// 				key: 'Cache-Control',
+	// 				value: 'no-store',
+	// 			},
+	// 		],
+	// 	},
+	// ],
 };
 
 module.exports = nextConfig;
