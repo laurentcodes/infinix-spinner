01c71511c3d9412581e74f9ad4902d2f82e1bc82
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-03 16:34:45 +0100

    fix: edit home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { TextInput, Button } from 'flowbite-react';
 
 export default function Home() {
 	const router = useRouter();
@@ -11,9 +12,9 @@ export default function Home() {
 	const [phone, setPhone] = useState(null);
 
 	return (
-		<main className='h-screen p-5 flex justify-evenly items-center overflow-hidden'>
+		<main className='h-screen p-5 flex flex-col md:flex-row justify-evenly items-center overflow-hidden'>
 			<div className='flex flex-col items-center'>
-				<h4 className='font-bold text-2xl'>Try Your Luck</h4>
+				<h4 className='font-bold text-3xl'>Try Your Luck</h4>
 
 				<img
 					src='/assets/screenshot.png'
@@ -21,6 +22,7 @@ export default function Home() {
 					width={450}
 				/>
 			</div>
+
 			<div className='flex flex-col items-center'>
 				<img
 					src={'/assets/infinix-logo.jpeg'}
@@ -30,28 +32,28 @@ export default function Home() {
 				/>
 
 				<div className='w-80 md:w-96 flex flex-col'>
-					<input
-						type='text'
-						id='email'
-						className='bg-white mb-3 block w-full p-2.5 border rounded-lg outline-none focus:border-2 focus:border-green-500 ease-linear duration-200'
+					<TextInput
+						className='mb-2'
+						id='email1'
 						placeholder='Enter Email'
-						value={email}
 						required
+						type='email'
+						value={email}
 						onChange={(e) => setEmail(e.target.value)}
 					/>
 
-					<input
-						type='number'
+					<TextInput
+						className='mb-2'
 						id='phone'
-						className='bg-white block w-full p-2.5 border rounded-lg outline-none focus:border-2 focus:border-green-500 ease-linear duration-200'
 						placeholder='Enter Phone Number'
-						value={phone}
 						required
+						type='number'
+						value={phone}
 						onChange={(e) => setPhone(e.target.value)}
 					/>
 
 					<button
-						className='p-2 mt-3 bg-green-500 rounded-lg w-full text-white uppercase font-bold'
+						className='p-2 mt-3 bg-green-400 rounded-lg w-full text-white uppercase'
 						onClick={() => router.push(`/spin?email=${email}&phone=${phone}`)}
 					>
 						Continue to Spin
