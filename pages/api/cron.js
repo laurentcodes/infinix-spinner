388c5a1e7987c0a15ca153af6545279e1adb69e8
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-04 17:43:22 +0100

    test cron

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -9,18 +9,18 @@ connectDB();
 
 const handler = async (req, res) => {
 	// Define cron job here
-	cron.schedule('0 0 * * *', async () => {
-		// Run at 00:00 every day
-		console.log('Running myFunction at 00:00 every day.');
-
-		await Item.updateMany({}, { count: 0 });
-	});
-
-	// cron.schedule('*/5 * * * * *', async () => {
-	// 	console.log('Running every 5 seconds lau.');
+	// cron.schedule('0 0 * * *', async () => {
+	// 	// Run at 00:00 every day
+	// 	console.log('Running myFunction at 00:00 every day.');
 
 	// 	await Item.updateMany({}, { count: 0 });
 	// });
+
+	cron.schedule('*/5 * * * * *', async () => {
+		console.log('Running every 5 seconds lau.');
+
+		await Item.updateMany({}, { count: 0, totalCount: 0 });
+	});
 
 	// Send a response to the client
 	res.status(200).json({
