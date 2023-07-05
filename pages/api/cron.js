ce68942b6f2db7c7947184c400fef1d91fb168ab
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-05 08:16:16 +0100

    test cron

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -14,27 +14,27 @@ const handler = async (req, res) => {
 	// 	await Item.updateMany({}, { count: 1 - 1, totalCount: 1 - 1 });
 	// });
 
+	// Define cron job here
+	// cron.schedule('0 0 * * *', async () => {
+	// 	// Run at 00:00 every day
+	// 	console.log('Running at 00:00 every day.');
+
+	// 	await Item.updateMany({}, { count: 1 - 1 });
+	// });
+
+	// cron.schedule('0 0 */23 * *', async () => {
+	// 	// Run every 23 hours
+	// 	console.log('Running every 23 hours.');
+
+	// 	await Item.updateMany({}, { count: 15, totalCount: 24 });
+	// });
+
 	if (connectRes.status === 200) {
-		// Define cron job here
-		// cron.schedule('0 0 * * *', async () => {
-		// 	// Run at 00:00 every day
-		// 	console.log('Running at 00:00 every day.');
-
-		// 	await Item.updateMany({}, { count: 1 - 1 });
-		// });
-
 		cron.schedule('*/5 * * * * *', async () => {
 			console.log('Running every 5 seconds lau.');
 
 			await Item.updateMany({}, { count: 123, totalCount: 124 });
 		});
-
-		// cron.schedule('0 0 */23 * *', async () => {
-		// 	// Run every 23 hours
-		// 	console.log('Running every 23 hours.');
-
-		// 	await Item.updateMany({}, { count: 15, totalCount: 24 });
-		// });
 
 		// Send a response to the client
 		res.status(200).json({
