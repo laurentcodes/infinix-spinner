a922987317f1c9d7a5dc4fee5490bd15f5b6d9db
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-05 09:27:07 +0100

    fix

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -8,32 +8,13 @@ const Item = require('./models/Item');
 const connectRes = await connectDB();
 
 const handler = async (req, res) => {
-	// cron.schedule('*/5 * * * * *', async () => {
-	// 	console.log('Running every 5 seconds lau.');
-
-	// 	await Item.updateMany({}, { count: 1 - 1, totalCount: 1 - 1 });
-	// });
-
-	// Define cron job here
-	// cron.schedule('0 0 * * *', async () => {
-	// 	// Run at 00:00 every day
-	// 	console.log('Running at 00:00 every day.');
-
-	// 	await Item.updateMany({}, { count: 1 - 1 });
-	// });
-
-	// cron.schedule('0 0 */23 * *', async () => {
-	// 	// Run every 23 hours
-	// 	console.log('Running every 23 hours.');
-
-	// 	await Item.updateMany({}, { count: 15, totalCount: 24 });
-	// });
-
 	if (connectRes.connected === 1) {
-		cron.schedule('*/5 * * * * *', async () => {
-			console.log('Running every 5 seconds lau.');
+		// Define cron job here
+		cron.schedule('0 0 * * *', async () => {
+			// Run at 00:00 every day
+			console.log('Running at 00:00 every day.');
 
-			await Item.updateMany({}, { count: 0, totalCount: 0 });
+			await Item.updateMany({}, { count: 0 });
 		});
 	}
 
