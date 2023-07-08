1190864ffcffc9fd9b3ce9ab3dbf8005d598c207
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-08 02:41:29 +0100

    fix

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -5,11 +5,11 @@ const Item = require('./models/Item');
 // Connect database
 await connectDB();
 
-const handler = async (req, res) => {
+const handler = (req, res) => {
 	// Run at 00:00 every day
 	console.log('Running at 00:00 every day.');
 
-	await Item.updateMany({}, { count: 0 });
+	Item.updateMany({}, { count: 0 });
 
 	res.status(200).json({
 		message: 'Cron job scheduled successfully.',
