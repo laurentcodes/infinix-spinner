ba6b6702fe50087e68150c1ba5d3cdccf9a76eec
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-08 02:29:16 +0100

    fix

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,31 +1,15 @@
 const connectDB = require('../../config/db');
 
-const cron = require('node-cron');
-
 const Item = require('./models/Item');
 
 // Connect database
-const connectRes = await connectDB();
+await connectDB();
 
 export default async function handler(req, res) {
-	// if (connectRes.connected === 1) {
-	// 	// Define cron job here
-	// 	cron.schedule('0 0 * * *', async () => {
-	// 		// Run at 00:00 every day
-	// 		console.log('Running at 00:00 every day.');
+	// Run at 00:00 every day
+	console.log('Running at 00:00 every day.');
 
-	// 		await Item.updateMany({}, { count: 0 });
-	// 	});
-	// }
-	if (connectRes.connected === 1) {
-		// Run at 02:25 every day
-		console.log('Running at 02:25 every day.');
-
-		await Item.updateMany({}, { count: 0 });
-	}
-
-	// Send a response to the client
-	console.log('CRON SCHEDULED');
+	await Item.updateMany({}, { count: 0 });
 
 	res.status(200).json({
 		message: 'Cron job scheduled successfully.',
